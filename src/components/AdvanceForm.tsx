ee3a1b356bffc46fbe5f60d8c0748cd9abf59c0a
Chetan-Vishwakarma <vishwakarmachetan569@gmail.com>
2024-06-18 19:14:54 +0530

    Responsiveness tested

--- src/components/AdvanceForm.tsx
+++ src/components/AdvanceForm.tsx
@@ -8,9 +8,9 @@ import SignUpForm from "./SignUpForm";
 export default function AdvanceForm() {
     const [isSignIn, setIsSignIn] = useState<boolean>(false);
     return (
-        <Stack justifyContent={'center'} alignItems={'center'} direction={'row'} height={'100vh'}>
-            <Stack direction={'row'} sx={{ border: '1px solid grey', width: '700px', height: '456px' }}>
-                <Box sx={{ width: '50%' }}>
+        <Stack justifyContent={'center'} alignItems={'center'} direction={'row'} height={{xs:'auto',sm:'auto',lg:'100vh',md:'100vh'}}>
+            <Stack direction={'row'} sx={{ border: '1px solid grey', width: {xs:'80%',lg:'700px',md:'700px',sm:'90%'} }}>
+                <Box sx={{ width: {xs:'100%',sm:'50%',lg:'50%',md:'50%'} }}>
                     {
                         isSignIn
                             ?
@@ -19,7 +19,7 @@ export default function AdvanceForm() {
                             <SignUpForm setIsSignIn={setIsSignIn} isSignIn={isSignIn} />
                     }
                 </Box>
-                <Box sx={{ width: '50%', height: 'auto' }}>
+                <Box sx={{ width: '50%', height: 'auto', display: {xs:'none',sm:'block',lg:'block',md:'block'}}}>
                     <img style={{ height: '100%' }} src="https://t4.ftcdn.net/jpg/02/00/90/53/360_F_200905394_2u1hKNTSawkcR6N1X0aX0PiSBR1tvUMn.jpg" />
                 </Box>
             </Stack>
