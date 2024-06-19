c276725879413adba218f9ad01b35d173f8dcc36
Chetan-Vishwakarma <vishwakarmachetan569@gmail.com>
2024-06-19 14:49:48 +0530

    Worked on some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import YupBasicValidationForm from "@/components/YupBasicValidationForm";
 
 export default async function Home() {
   return (
-    <div style={{backgroundImage: 'linear-gradient(to top, #a8edea 0%, #fed6e3 100%)'}}>
+    <div style={{height:'100vh',display: 'flex',flexDirection:'column',justifyContent:'center',backgroundImage: 'linear-gradient(to top, #a8edea 0%, #fed6e3 100%)'}}>
       {/* <BasicForm/> */}
       {/* <MUIBasicForm/> */}
       {/* <YupBasicValidationForm/> */}
